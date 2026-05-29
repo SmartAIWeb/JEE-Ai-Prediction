a2b0ae448612268e1b6e2d9f8393fb32cb25817e
Nomix17 <izayrit12345678@gmail.com>
2026-05-29 23:22:40 +0100

    fix: fix schema relations

--- database/schema.sql
+++ database/schema.sql
@@ -1,26 +1,23 @@
 CREATE DATABASE IF NOT EXISTS prediction_app_db;
 USE prediction_app_db;
 
-CREATE TABLE History (
-  history_id INT AUTO_INCREMENT,
-  user_id INT,
-  input_data JSON,
-  prediction_res JSON,
-  date DATETIME DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (history_id)
-);
-
 CREATE TABLE Users (
-  user_id INT AUTO_INCREMENT,
-  history_id INT,
+  user_id INT AUTO_INCREMENT PRIMARY KEY,
   firstName VARCHAR(100),
   lastName VARCHAR(100),
   age INT,
   gender ENUM('male', 'female'),
-  email VARCHAR(100),
+  email VARCHAR(100) UNIQUE,
   password VARCHAR(255),
-  role ENUM('user', 'admin'),
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (user_id),
-  FOREIGN KEY (history_id) REFERENCES History(history_id)
+  role ENUM('user', 'admin') DEFAULT 'user',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE History (
+  history_id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT,
+  input_data JSON,
+  prediction_res JSON,
+  date DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
 );
